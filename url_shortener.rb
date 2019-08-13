666901ee6086f6c67f25ba8f2366504dca95ea08
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-08-13 06:09:03 +0100

    Add sessions to access params accross different http req

--- url_shortener.rb
+++ url_shortener.rb
@@ -2,14 +2,22 @@ require 'securerandom'
 require 'sinatra/base'
 
 class URL_Shortener < Sinatra::Base
+
+enable :sessions
+
   get '/' do
     erb :landing_page
   end
 
   post '/' do
     @url = params[:url]
+    session[:url] = @url
     @short_url = shorten_url()
     erb :results_page
+  end
+
+  get '/:url' do
+    redirect session[:url]
   end
 
   def shorten_url
